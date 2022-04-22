e3b2b9002cb3f2f9832e8f3396f670b1c93eefc6
«Nikita_Boiko» <nikita-bojko@yandex.ru>
2022-04-22 20:35:13 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,10 @@
+﻿double X1 = 3;
+double X2 = 2;
+double Y1 = 6;
+double Y2 = 1;
+double Z1 = 8;
+double Z2 = -7;
+
+double length = Math.Sqrt(Math.Pow(X2-X1,2) + Math.Pow(Y2-Y1,2) + Math.Pow(Z2-Z1,2));
+
+Console.WriteLine (length);
