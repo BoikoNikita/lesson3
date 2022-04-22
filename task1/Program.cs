32ab524addf9a90839697c9ed2bcf1fd10b7a6df
«Nikita_Boiko» <nikita-bojko@yandex.ru>
2022-04-22 22:45:57 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+int N = int.Parse(Console.ReadLine());
+
+if (N/10000 == N%10 && (N/1000)%10==(N%100)/10)
+{
+   Console.WriteLine("Это число палиндром!");
+}
+else
+{
+    Console.WriteLine("Это число не палиндром!");
+}
