bd6fbb447818ee5805a7762642a19fd5d370d8de
«Nikita_Boiko» <nikita-bojko@yandex.ru>
2022-04-22 21:35:35 +0300

    task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,9 +1,15 @@
-﻿double X1 = 3;
-double X2 = 2;
-double Y1 = 6;
-double Y2 = 1;
-double Z1 = 8;
-double Z2 = -7;
+﻿Console.WriteLine("Введите X1: ");
+int X1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите X2: ");
+int X2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите Y1: ");
+int Y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите Y2: ");
+int Y2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите Z1: ");
+int Z1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите Z2: ");
+int Z2 = int.Parse(Console.ReadLine());
 
 double length = Math.Sqrt(Math.Pow(X2-X1,2) + Math.Pow(Y2-Y1,2) + Math.Pow(Z2-Z1,2));
 
